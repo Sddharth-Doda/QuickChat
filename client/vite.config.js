025adcfd2300c547478d133816d313b18c402934
Siddharth-Doda <siddharthdoda04@gmail.com>
2025-07-18 16:32:34 +0530

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5001',
+      '/api': 'https://quickchat-backend-n5r2.onrender.com',
     },
   },
 });
