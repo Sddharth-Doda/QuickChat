a0d70e9f65ce79b7871c5faf4801321773dd10a0
Siddharth-Doda <siddharthdoda04@gmail.com>
2025-07-18 18:44:55 +0530

    updated cors

--- server/server.js
+++ server/server.js
@@ -17,7 +17,8 @@ app.use(cors({
   origin : process.env.FRONTEND_URL,
   credentials : true,
   methods : ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  // allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ["Content-Type", "token"], 
 }));
 
 app.use(
